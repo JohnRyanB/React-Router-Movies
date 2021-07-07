2e6e7e69bb85fab85dad66b953a6d24764cbf3b0
John-Blackburn <JRBbusinesscontact@gmail.com>
2021-07-07 18:37:34 -0400

    started

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
 
 export default function SavedList(props) {
-  return (
-    <div className="saved-list">
-      <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
-        <span className="saved-movie">{movie.title}</span>
-      ))}
-      <div className="home-button">Home</div>
-    </div>
-  );
+	return (
+		<div className="saved-list">
+			<h3>Saved Movies:</h3>
+			{props.list.map((movie) => (
+				<span className="saved-movie">{movie.title}</span>
+			))}
+			<div to="/" className="home-button">
+				Home
+			</div>
+		</div>
+	);
 }
